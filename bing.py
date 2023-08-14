3ce7afef3439997d2aa51b9b0a3ac3cfa16056e3
a1564471347 <65760851+a1564471347@users.noreply.github.com>
2023-08-14 16:58:20 +0800

    Add files via upload

--- bing.py
+++ bing.py
@@ -1,9 +1,10 @@
 """
 Edge必应自动搜索赚积分
 使用Edge浏览器打开必应 https://rewards.bing.com/ F12抓取Cookie即可  正确的CK格式是以MUID=xxxxxxx开头的
-当前版本：v1.2
+当前版本：v1.3
 
-变量名：bingCK  多账号换行 
+变量名：bingCK  多账号换行  
+bingDetectionStop 是否检测到积分未增长自动停止任务  默认为true  不需要该功能则额外定义变量，值为false
 
 如果执行的发现积分不增长，且脚本上显示的积分跟实际不符，很有可能不是同一个账号的cookie，建议重新抓取。
 玄学报错目前无解，凑合用吧！
@@ -19,7 +20,7 @@ import urllib.parse
 from time import strftime
 
 old_Balance = 0
-
+bingDetectionStop = os.getenv("bingDetectionStop")
 #获取用户信息
 def getDashboard(bingCK):
     try:
@@ -80,6 +81,7 @@ def bing_rewards(q_str,bingCK,isPc):
 #随机生成一个汉字
 def get_random_char():
     # 汉字编码的范围是0x4e00 ~ 0x9fa5
+    random.seed()
     val = random.randint(0x4e00, 0x9fa5)
     # 转换为Unicode编码
     return chr(val)
@@ -93,6 +95,7 @@ def find(str,str_start,str_end):
 
 #执行     	
 def startMain(bingCK):
+    '''
     try:
         userDashboard_json = json.loads(getDashboard(bingCK))
         activeLevel = userDashboard_json['userStatus']['levelInfo']['activeLevel']
@@ -106,8 +109,18 @@ def startMain(bingCK):
     except Exception as e:
         activeLevel = 'Level2'
         printLog('当前等级','获取失败')
-
-    old_Balance = getBalance(bingCK)
+    '''
+    #根据当前积分判断用户组
+    start_Balance = getBalance(bingCK)
+    if int(start_Balance) < 500:
+        activeLevel = 'Level1'
+        printLog('当前用户组','第一阶段')
+        upBalance = getBalanceGap(start_Balance,500)
+        printLog('距离升级下一阶段',f'还需要{upBalance}积分')
+    else:
+        activeLevel = 'Level2'
+        printLog('当前用户组','第二阶段')
+    old_Balance = start_Balance
     if activeLevel == 'Level1':
         #pc端搜索10次
         for i in range(10):
@@ -115,10 +128,11 @@ def startMain(bingCK):
             printLog(f'电脑搜索第{i+1}次',bing_rewards(q_str,bingCK,1))
             new_Balance = getBalance(bingCK)
             printLog(f'积分',new_Balance)
-            if getBalanceGap(old_Balance,new_Balance) <= 0:
-                printLog('检测','积分未变动，停止运行！')
-                break
-            old_Balance = new_Balance    
+            if bingDetectionStop != 'false':
+                if getBalanceGap(old_Balance,new_Balance) <= 0:
+                    printLog('检测','积分未变动，停止运行！')
+                    break
+                old_Balance = new_Balance    
             rand = random.randint(3,5)
             time.sleep(rand)
     else:
@@ -128,10 +142,11 @@ def startMain(bingCK):
             printLog(f'电脑搜索第{i+1}次',bing_rewards(q_str,bingCK,1))
             new_Balance = getBalance(bingCK)
             printLog(f'积分',new_Balance)
-            if getBalanceGap(old_Balance,new_Balance) <= 0:
-                printLog('检测','积分未变动，停止运行！')
-                break
-            old_Balance = new_Balance  
+            if bingDetectionStop != 'false':
+                if getBalanceGap(old_Balance,new_Balance) <= 0:
+                    printLog('检测','积分未变动，停止运行！')
+                    break
+                old_Balance = new_Balance  
             rand = random.randint(3,5)
             time.sleep(rand)    
         #安卓端搜索20次    	
@@ -140,12 +155,16 @@ def startMain(bingCK):
             printLog(f'安卓搜索第{i+1}次',bing_rewards(q_str,bingCK,0))
             new_Balance = getBalance(bingCK)
             printLog(f'积分',new_Balance)
-            if getBalanceGap(old_Balance,new_Balance) <= 0:
-                printLog('检测','积分未变动，停止运行！')
-                break
-            old_Balance = new_Balance
+            if bingDetectionStop != 'false':
+                if getBalanceGap(old_Balance,new_Balance) <= 0:
+                    printLog('检测','积分未变动，停止运行！')
+                    break
+                old_Balance = new_Balance
             rand = random.randint(3,5)
             time.sleep(rand) 
+    end_Balance = getBalance(bingCK)
+    increase_Balance = getBalanceGap(start_Balance,end_Balance)
+    printLog('本次增加积分',f'{increase_Balance}')
 
 #计算积分增长数
 def getBalanceGap(old_Balance,new_Balance):
@@ -165,6 +184,8 @@ def printLog(title,msg):
    print(now+f' [{title}]:'+nvl(msg))
  	             
 if __name__ == '__main__':
+    if bingDetectionStop == '':
+        bingDetectionStop = 'true'
     bingCK = os.getenv("bingCK")
     cks = bingCK.split("\n")
     print(f"检测到{len(cks)}个账号,即将开始...")
